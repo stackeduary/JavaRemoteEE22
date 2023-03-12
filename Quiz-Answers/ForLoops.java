c896289b879ed63c41de0f0c17d599aa9a845d3d
Bill <stackeduary@gmail.com>
2023-03-12 13:35:17 +0200

    Updating Quiz 8 answers

--- Quiz-Answers/ForLoops.java
+++ Quiz-Answers/ForLoops.java
@@ -1,25 +1,5 @@
-## Quiz 8
-#### Saturday, Mar. 4, 2023
-
-1. Name the two types of exceptions in Java.
-  - checked
-    - developer is forced to handle
-  - unchecked
-    - dev does not have to have
-
-2. What are the two ways of handling exceptions in Java?
-  - using a try-catch block
-  - adding the exception to method signature of the form `... throws <type of exception here>`
-
-3. What are the two types of for-loops in Java?
-  - general form
-  - enhanced form (AKA `for-each` loop)
-
-4. Using one of the two types, print out all the years that had modern Summer Olympics: https://en.wikipedia.org/wiki/Summer_Olympic_Games#List_of_Summer_Olympic_Games (assume that the 2020 Olympics happened in their normally-scheduled year).
-
-5. Repeat #4 using the other type of for-loop.
-
-Suggestions: use IntelliJ for #4 and #5
+// Quiz 8
+// Saturday, Mar. 4, 2023
 
 import java.util.ArrayList;
 
